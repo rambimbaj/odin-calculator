c7722c6d8a9490dc15f0dae6194ac1765e8af128
rambimbaj <107889088+rambimbaj@users.noreply.github.com>
2024-03-23 20:09:59 +0100

    test

---
+++ js.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Error";
+  }
+  return a / b;
+}
+
+function operate(a, b, op) {
+    let result;
+  switch (op) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = subtract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+  }
+  return result;
+}
